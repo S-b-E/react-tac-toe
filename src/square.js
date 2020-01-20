d3cb5ada04c9cfddcda02a5da6d374ea9ded7e2b
S-b-E <105817+S-b-E@users.noreply.github.com>
2020-01-20 16:52:20 -0500

    Next steps

--- src/square.js
+++ src/square.js
@@ -1,16 +1,9 @@
 import React from 'react';
 
 export default class Square extends React.Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-        value: null,
-      };
-    }
-
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={() => this.props.onClick()}>
         {this.props.value}
       </button>
     );
